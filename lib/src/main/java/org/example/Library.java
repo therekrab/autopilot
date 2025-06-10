de61f14400f1661a3175dc273b2784e9b56d9514
Nolan Barker <nolanb414@yahoo.com>
2025-06-10 16:45:07 -0400

    Add WPILib dependencies; does NOT run
    It looks like something's going wrong with the JNI?

--- lib/src/main/java/org/example/Library.java
+++ lib/src/main/java/org/example/Library.java
@@ -4,7 +4,7 @@
 package org.example;
 
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
-    }
+  public boolean someLibraryMethod() {
+    return true;
+  }
 }
