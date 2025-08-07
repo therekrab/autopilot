ff6546bb48e663f751c47ed750e6d330f3b1f5eb
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2025-08-06 22:02:14 -0700

    fix APVelocity

--- lib/src/main/java/com/therekrab/autopilot/APVelocity.java
+++ lib/src/main/java/com/therekrab/autopilot/APVelocity.java
@@ -1,59 +1,64 @@
 package com.therekrab.autopilot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 /**
- * A helper class that converts a robot's ChassisSpeeds to a Translation2d for Autopilot calculations.
+ * A helper class that converts a robot's ChassisSpeeds and pose to a Translation2d for Autopilot calculations.
  */
 public class APVelocity {
   protected double vx;
   protected double vy;
-  protected double omega;
+  protected Rotation2d theta;
 
   /**
    *  Creates a new APVelocity with zero velocity and omega.
    * 
-   * <p> This constructor creates an APVelocity that will result in no movement when passed into Autopilot.calculate().
+   * <p> This constructor creates an blank APVelocity. This can be dangerous, as it will cause Autopilot to 
+   * assume the robot is not moving. If you are using APVelocity, you should always update it with the robot's current velocity.
    */
   public APVelocity() {
     vx = 0;
     vy = 0;
-    omega = 0;
+    theta = new Rotation2d();
   }
 
       /**
-   * Creates a new APVelocity from a given ChassisSpeeds.
+   * Creates a new APVelocity from a given ChassisSpeeds and robot pose.
    * 
    * <p> Autopilot utilizes Translation2d objects for velocity calculations, so this method allows users to skip creating a helper
-   * method to convert their robot's ChassisSpeeds to a Translation2d. If your drivetrain implementation doesn't have a method for accessing its ChassisSpeeds,
+   * method to convert their robot's ChassisSpeeds and pose to a Translation2d. If your drivetrain implementation doesn't have a method for accessing its ChassisSpeeds,
    * see the ChassisSpeeds documentation for more information on creating one.
    * 
    * @param robotRelativeSpeeds The current robot-relative ChassisSpeeds
+   * @param robotPose The current robot Pose2d
    * 
    */
 
-  public APVelocity(ChassisSpeeds robotRelativeSpeeds) {
+  public APVelocity(ChassisSpeeds robotRelativeSpeeds, Pose2d robotPose) {
     vx = robotRelativeSpeeds.vxMetersPerSecond;
     vy = robotRelativeSpeeds.vyMetersPerSecond;
-    omega = robotRelativeSpeeds.omegaRadiansPerSecond;  
+    theta = robotPose.getRotation();  
   } 
 
 
         /**
-   * Update this APVelocity with a new ChassisSpeeds.
+   * Update this APVelocity with a new ChassisSpeeds and robot pose.
    * 
    * <p> As Autopilot is intended to be called periodically, if you are using APVelocity, 
    * use this to update the APVelocity with the robot's current ChassisSpeeds.
    * 
+   * @param robotRelativeSpeeds The current robot-relative ChassisSpeeds
+   * 
    * @return a Translation2d used for Autopilot calculations
    */
 
-  public APVelocity updateSpeeds(ChassisSpeeds robotRelativeSpeeds) {
+  public APVelocity update(ChassisSpeeds robotRelativeSpeeds, Pose2d robotPose) {
     this.vx = robotRelativeSpeeds.vxMetersPerSecond;
     this.vy = robotRelativeSpeeds.vyMetersPerSecond;
-    this.omega = robotRelativeSpeeds.omegaRadiansPerSecond;
+    this.theta = robotPose.getRotation();
     return this;  
   }
 
@@ -69,7 +74,7 @@ public class APVelocity {
   public Translation2d get() {
     return new Translation2d(vx, vy).
     rotateBy(
-      new Rotation2d(omega)
+      theta
       );
   }
 
